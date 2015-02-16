44f3d72babf1e4de73d0f8ed4c7e816e43257660
PaddyK <patrick.kalmbach@web.de>
2015-02-16 10:55:59 +0100

    initial commit on my branch, working on feature extraction script

---
+++ feature_extraction.ps1
@@ -0,0 +1,4 @@
+#opensmile -C G:\Tools\openSMILE-2.1.0\config\demo\demo1_energy.conf -I G:\Documents\Dropbox\Project\Aufnahmen\clap\clap_reading_01.wav -O G:\Documents\Dropbox\Project\Aufnahmen\clap\test.csv
+
+
+opensmile -C G:\Documents\GitHub\ML-Audio-Project\mfcc.conf -I G:\Documents\GitHub\ML-Audio-Project\clap_reading_01.wav
