4e3357be18faea881e6b6844d18c09084e57ae99
PaddyK <patrick.kalmbach@web.de>
2015-02-16 16:35:00 +0100

    pretty printing

--- java/src/MlAudioProject.java
+++ java/src/MlAudioProject.java
@@ -20,22 +20,23 @@ public class MlAudioProject {
         
         do {
         	try {
-	            System.out.println("Command\tDescription");
-	            System.out.println("1\t\tSet target (arff file containing all sample values) path");
-	            System.out.println("2\t\tSet path to directory containing clap arffs");
-	            System.out.println("3\t\tSet path to directoyr containing noclap arffs");
-	            System.out.println("4\t\tCopy content of clap and noclap arffs into one file (target)." + nl
-	                 + " Paths to directories containing clap and noclap arffs must specified as well as target file." + nl
-	                 + " current:" + nl
-	                 + "\tclap: " + clap + nl
-	                 + "\tnoclap: " + noclap + nl
-	                 + "\ttarget: " + target);
-	            System.out.println("5\t\tPerform GridSearch for LibSVM with radial kernel" + nl
-	                 + " Path to target (arff file containing all values) must be specified. " + nl
-	                 + " current: " + target);
+	            System.out.println("Cmd\tDescription");
+	            System.out.println("1\tSet target (arff file containing all sample values) path");
+	            System.out.println("2\tSet path to directory containing clap arffs");
+	            System.out.println("3\tSet path to directoyr containing noclap arffs");
+	            System.out.println("4\tCopy content of clap and noclap arffs into one file (target)." + nl
+	                 + "\tPaths to directories containing clap and noclap arffs must specified as well " + nl
+                     + "\tas target file." + nl
+	                 + "\t current:" + nl
+	                 + "\t\tclap: " + clap + nl
+	                 + "\t\tnoclap: " + noclap + nl
+	                 + "\t\ttarget: " + target);
+	            System.out.println("5\tPerform GridSearch for LibSVM with radial kernel" + nl
+	                 + "\t Path to target (arff file containing all values) must be specified. " + nl
+	                 + "\t current: " + target);
 	            System.out.println("q\tExit");
 	            
-	            System.out.print("Your choice: ");
+	            System.out.println("Your choice: ");
 	            input = reader.readLine();
 	
 	            if(input.equals("q")) {
